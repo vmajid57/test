1b7276432360d0297c5a6f367470393bd9ed2f01
majid <majidvayeghani@gmail.com>
2022-07-11 18:58:36 +0430

    pass source and price to template for show on front. wrote coments on code

--- money/category/views.py
+++ money/category/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import InputExpence
+from .models import Category
+from django.db.models import Sum
 
 def add_expence(request):
 	if request.method == 'POST':
 		form = InputExpence(request.POST)  # instantiate a new form here
 
 		if form.is_valid():
-			# source = form.cleaned_data['source'],
-			# price = form.cleaned_data['price']
+			template = loader.get_template('expense.html')
 			form.save()
-			return HttpResponseRedirect('/expense/')
+			
+			sumOfPrice = Category.objects.aggregate(Sum('price')) #sum of all prices
+			
+			# for create object from source and price
+			arr = []
+			context = {}
+			
+			for item in Category.objects.all():
+				# every row > {'source' : price}
+				temp = {}
+				temp[item.source] = item.price
+				arr.append(temp) #every row append to array
+			
+			#final dic for pass to template. i don't know why i do this :))
+			#cat is name for key
+			context['cat'] = arr 
+			return HttpResponse(template.render(context, request))        
 	else:
 		form = InputExpence()
 	
