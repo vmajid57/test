889037bddee1a92af308c2c88b3420308afb48cc
majid <majidvayeghani@gmail.com>
2022-07-21 14:48:19 +0430

    change way send dict to template

--- money/category/views.py
+++ money/category/views.py
@@ -15,32 +15,18 @@ def add_expense(request):
 			form.save()
 			
 			sumofprice = Category.objects.aggregate(Sum('price'))  # sum of all prices
-			# for create object from source and price
-			arr = []
-			for item in Category.objects.all():
-				# every row > {'source' : price}
-				temp = {item.source: item.price}
-				arr.append(temp)  # every row append to array
-			
+
 			# TODO: think about why template just give dict
-			# cat is name for key
 			result = Category.objects.values('source').annotate(total_price=Sum('price'))
 
-			print(result)
-			return HttpResponse(template.render({'form': form, 'cat': arr}, request))        
+			return HttpResponse(template.render({'form': form, 'cat': Category.objects.all()}, request))
 	else:
 		form = InputExpense()
-		# TODO: do this for loop once. this loop repeat in if and else block.
-		arr = []
-		for item in Category.objects.all():
-			# every row > {'source' : price}
-			temp = {item.source: item.price}
-			arr.append(temp)
+
 		result = Category.objects.values('source').annotate(total_price=Sum('price'))
-		print(result)
 
 	# TODO: why this line was out of the block? i don't know why
-	return render(request, 'expense.html', {'form': form, 'cat': arr})  # pass that form to the template
+	return render(request, 'expense.html', {'form': form, 'cat': Category.objects.all()})  # pass that form to the template
 
 
 def thanks(request):
