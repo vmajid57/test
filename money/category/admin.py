55fa8f1535d2c8f592d20563efa2bd3717bc90a8
majid <majidvayeghani@gmail.com>
2022-07-21 15:16:14 +0430

    change detail of admin page

--- money/category/admin.py
+++ money/category/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models import Category
 
 
-# Register your models here.
-@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('source', 'price')
-    
+    list_display = ("source", "price", "publish", "created_at")
+    search_fields = ("source", "price", "created_at")
+    list_filter = ("source", "price", "created_at")
+    ordering = ['source', 'created_at']
+# Register your models here.
+admin.site.register(Category, CategoryAdmin)
 
--- money/category/models.py
+++ money/category/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 class Category(models.Model):
     source = models.CharField(max_length=30)
     price = models.IntegerField()
+    publish = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.source
